8fc69220b811414f06075a1db2c5173315a2479d
Denys Bzenko <dbzenko@kse.org.ua>
2023-10-09 23:29:01 +0300

    Not work all

--- program/script/index.js
+++ program/script/index.js
@@ -1,44 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
     class TodoItem {
-        constructor(text, date) {
+        constructor(text) {
             this.text = text;
-            this.date = date;
+            this.date = new Date();
             this.completed = false;
             this.deleted = false;
         }
     }
 
     class TodoItemPremium extends TodoItem {
-        constructor(text, date, icon) {
-            super(text, date);
-            this.icon = icon; // URL до зображення/іконки
+        constructor(text, icon) {
+            super(text);
+            this.icon = icon; // URL of the image/icon
         }
     }
 
-    const taskList = document.getElementById('taskList');
-    const newTaskInput = document.getElementById('newTask');
-    const addTaskButton = document.getElementById('addTask');
-    const removeCompletedButton = document.getElementById('removeCompleted');
-    const removeAllButton = document.getElementById('removeAll');
-    const sortAscButton = document.getElementById('sortAsc');
-    const sortDescButton = document.getElementById('sortDesc');
-    const clearStorageButton = document.getElementById('clearStorage');
-    const pickRandomButton = document.getElementById('pickRandom');
-    const iconFileInput = document.getElementById('iconFile');
+    const DOM = {
+        taskList: document.getElementById('taskList'),
+        newTaskInput: document.getElementById('newTask'),
+        addTaskButton: document.getElementById('addTask'),
+        removeCompletedButton: document.getElementById('removeCompleted'),
+        removeAllButton: document.getElementById('removeAll'),
+        sortAscButton: document.getElementById('sortAsc'),
+        sortDescButton: document.getElementById('sortDesc'),
+        clearStorageButton: document.getElementById('clearStorage'),
+        pickRandomButton: document.getElementById('pickRandom'),
+        iconFileInput: document.getElementById('iconFile')
+    };
 
     let tasks = [];
-    let sortDirection = 'desc'; // за замовчуванням
+    let sortDirection = 'desc';
 
     function formatDate(date) {
         return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
     }
 
     function sortTasks() {
-        if (sortDirection === 'desc') {
-            tasks.sort((a, b) => b.date - a.date);
-        } else {
-            tasks.sort((a, b) => a.date - b.date);
-        }
+        tasks.sort((a, b) => sortDirection === 'desc' ? b.date - a.date : a.date - b.date);
     }
 
     function saveTasksToLocalStorage() {
@@ -49,71 +47,18 @@ document.addEventListener('DOMContentLoaded', function() {
     function loadTasksFromLocalStorage() {
         const storedTasks = localStorage.getItem('tasks');
         const storedSortDirection = localStorage.getItem('sortDirection');
-        if (storedTasks) {
-            tasks = JSON.parse(storedTasks);
-        }
-        if (storedSortDirection) {
-            sortDirection = storedSortDirection;
-        }
+        tasks = storedTasks ? JSON.parse(storedTasks) : [];
+        sortDirection = storedSortDirection || 'desc';
     }
 
     function pickRandomTask() {
         const activeTasks = tasks.filter(task => !task.completed && !task.deleted);
-        if (activeTasks.length) {
-            const randomTask = activeTasks[Math.floor(Math.random() * activeTasks.length)];
-            alert(`Your task for now: ${randomTask.text}`);
-        } else {
-            alert('No active tasks available.');
-        }
+        const randomTask = activeTasks[Math.floor(Math.random() * activeTasks.length)];
+        alert(randomTask ? `Your task for now: ${randomTask.text}` : 'No active tasks available.');
     }
 
-    sortAscButton.addEventListener('click', function() {
-        sortDirection = 'asc';
-        sortTasks();
-        renderTasks();
-        saveTasksToLocalStorage();
-    });
-
-    sortDescButton.addEventListener('click', function() {
-        sortDirection = 'desc';
-        sortTasks();
-        renderTasks();
-        saveTasksToLocalStorage();
-    });
-
-    clearStorageButton.addEventListener('click', function() {
-        localStorage.clear();
-        tasks = [];
-        renderTasks();
-    });
-
-    pickRandomButton.addEventListener('click', pickRandomTask);
-
-    removeCompletedButton.addEventListener('click', function() {
-        tasks = tasks.filter(task => !task.completed && !task.deleted);
-        renderTasks();
-    });
-
-    taskList.addEventListener('dblclick', function(e) {
-        if (e.target.tagName === 'SPAN' && !e.target.parentElement.classList.contains('deleted')) {
-            const index = parseInt(e.target.nextElementSibling.getAttribute('data-index'));
-            newTaskInput.value = tasks[index].text;
-            newTaskInput.focus();
-            newTaskInput.addEventListener('keydown', function(event) {
-                if (event.key === 'Enter') {
-                    tasks[index].text = newTaskInput.value.trim();
-                    tasks[index].date = new Date();
-                    newTaskInput.value = '';
-                    renderTasks();
-                } else if (event.key === 'Escape') {
-                    newTaskInput.value = '';
-                }
-            });
-        }
-    });
-
     function renderTasks() {
-        taskList.innerHTML = '';
+        DOM.taskList.innerHTML = '';
         sortTasks();
         tasks.forEach((task, index) => {
             const li = document.createElement('li');
@@ -121,61 +66,71 @@ document.addEventListener('DOMContentLoaded', function() {
             if (task.icon) {
                 li.innerHTML += `<img src="${task.icon}" alt="Icon" width="50">`;
             }
-            if (task.deleted) {
-                li.classList.add('deleted');
-            }
-            if (task.completed) {
-                li.classList.add('completed');
-            }
-            li.addEventListener('click', function(e) {
-                if (e.target.tagName !== 'BUTTON') {
-                    tasks[index].completed = !tasks[index].completed;
-                    renderTasks();
-                }
-            });
-            taskList.appendChild(li);
-        });
-        const deleteButtons = document.querySelectorAll('.delete');
-        deleteButtons.forEach(button => {
-            button.addEventListener('click', function() {
-                const index = parseInt(button.getAttribute('data-index'));
-                tasks[index].deleted = true;
-                renderTasks();
-            });
+            li.classList.toggle('deleted', task.deleted);
+            li.classList.toggle('completed', task.completed);
+            DOM.taskList.appendChild(li);
         });
     }
 
-    addTaskButton.addEventListener('click', function() {
-        const taskText = newTaskInput.value.trim();
-        const iconURL = iconFileInput.value;
-        if (taskText) {
-            const newTask = iconURL ? new TodoItemPremium(taskText, new Date(), iconURL) : new TodoItem(taskText, new Date());
-            tasks.push(newTask);
-            newTaskInput.value = '';
-            iconFileInput.value = '';
+    function initEventListeners() {
+        DOM.sortAscButton.addEventListener('click', function() {
+            sortDirection = 'asc';
             renderTasks();
             saveTasksToLocalStorage();
-        }
-    });
+        });
 
-    removeAllButton.addEventListener('click', function() {
-        if (tasks.some(task => !task.completed)) {
-            const confirmDelete = confirm('Are you sure you want to delete all tasks, including uncompleted ones?');
-            if (confirmDelete) {
-                tasks = [];
-                renderTasks();
-            }
-        } else {
+        DOM.sortDescButton.addEventListener('click', function() {
+            sortDirection = 'desc';
+            renderTasks();
+            saveTasksToLocalStorage();
+        });
+
+        DOM.clearStorageButton.addEventListener('click', function() {
+            localStorage.clear();
             tasks = [];
             renderTasks();
-        }
-    });
+        });
+
+        DOM.pickRandomButton.addEventListener('click', pickRandomTask);
+
+        DOM.removeCompletedButton.addEventListener('click', function() {
+            tasks = tasks.filter(task => !task.completed);
+            renderTasks();
+        });
+
+        DOM.taskList.addEventListener('click', function(e) {
+            if (e.target.classList.contains('delete')) {
+                const index = parseInt(e.target.getAttribute('data-index'));
+                tasks[index].deleted = true;
+                renderTasks();
+            } else if (e.target.tagName === 'LI' || e.target.tagName === 'SPAN') {
+                const li = e.target.closest('li');
+                const index = Array.from(DOM.taskList.children).indexOf(li);
+                tasks[index].completed = !tasks[index].completed;
+                renderTasks();
+            }
+        });
+
+        DOM.addTaskButton.addEventListener('click', function() {
+            const taskText = DOM.newTaskInput.value.trim();
+            if (taskText) {
+                const iconURL = DOM.iconFileInput.value;
+                const newTask = iconURL ? new TodoItemPremium(taskText, iconURL) : new TodoItem(taskText);
+                tasks.push(newTask);
+                DOM.newTaskInput.value = '';
+                DOM.iconFileInput.value = '';
+                renderTasks();
+                saveTasksToLocalStorage();
+            }
+        });
+
+        DOM.removeAllButton.addEventListener('click', function() {
+            tasks = [];
+            renderTasks();
+        });
+    }
 
     loadTasksFromLocalStorage();
     renderTasks();
+    initEventListeners();
 });
-
-    
-    
-
-    
